62513085fc77160af4c316715118ca3bede220f6
Brad Crittenden <brad.crittenden@gmail.com>
2023-06-23 15:42:44 -0400

    Added more tests.

--- check_my_certs/check_my_certs.py
+++ check_my_certs/check_my_certs.py
@@ -1,9 +1,9 @@
 """Main module."""
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from urllib.request import ssl, socket
 
-__ALL__ = ("check",)
+__ALL__ = ("check", "check_cert", "DATEFORMAT")
 
 DATE_FORMAT = "%b %d %H:%M:%S %Y %Z"
 
@@ -25,7 +25,9 @@ def parse_file(fn):
 
 def parse_date(date_str):
     """Parse the date string and return date object."""
-    return datetime.strptime(date_str, DATE_FORMAT)
+    naive = datetime.strptime(date_str, DATE_FORMAT)
+    tzaware = naive.replace(tzinfo=timezone.utc)
+    return tzaware
 
 
 def check_cert(host, port, warning_date):
@@ -37,15 +39,16 @@ def check_cert(host, port, warning_date):
                 cert = ssl_sock.getpeercert()
                 not_before = parse_date(cert["notBefore"])
                 not_after = parse_date(cert["notAfter"])
-                if not_after < warning_date or not_before > datetime.now():
+                if not_after < warning_date or not_before > datetime.now(
+                    tz=timezone.utc
+                ):
                     flag = "❌"
                 else:
                     flag = "✅"
     except TimeoutError:
         flag = "❌⏰"
         not_after = "N/A"
-
-    print_result(f"{host}:{port}", str(not_after), flag)
+    return not_after, flag
 
 
 def print_result(url, expiry, flag):
@@ -56,6 +59,7 @@ def print_result(url, expiry, flag):
 
 def check(fn, days):
     """Run the certificate checker."""
-    warning_date = datetime.now() + timedelta(days=days)
+    warning_date = datetime.now(tz=timezone.utc) + timedelta(days=days)
     for host, port in parse_file(fn):
-        check_cert(host, port, warning_date)
+        not_after, flag = check_cert(host, port, warning_date)
+        print_result(f"{host}:{port}", str(not_after), flag)
--- tests/test_check_my_certs.py
+++ tests/test_check_my_certs.py
@@ -3,14 +3,16 @@
 """Tests for `check_my_certs` package."""
 
 
-import unittest
+from datetime import datetime, timedelta, timezone
+from unittest import mock, TestCase
+
+
 from click.testing import CliRunner
 
-from check_my_certs import check_my_certs
-from check_my_certs import cli
+from check_my_certs import check_my_certs, cli
 
 
-class TestCheck_my_certs(unittest.TestCase):
+class TestCheck_my_certs(TestCase):
     """Tests for `check_my_certs` package."""
 
     def setUp(self):
@@ -19,6 +21,23 @@ class TestCheck_my_certs(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        help_result = runner.invoke(cli.main, ["--help"])
+        self.assertEqual(0, help_result.exit_code)
+        help_output = help_result.output
+        self.assertRegex(help_output, r"--help\s+Show this message and exit")
+        self.assertRegex(
+            help_output,
+            r"-f, --filename TEXT\s+File listing sites to check.  "
+            r"\[default: sites.txt\]",
+        )
+        self.assertRegex(
+            help_output,
+            r"-d, --days INTEGER\s+Days until expiry for warning.  \[default: 14\]",
+        )
+
     def test_parse_file(self):
         """Test parse_file returns an expected list."""
         res = list(check_my_certs.parse_file("tests/files/sites.txt"))
@@ -26,12 +45,34 @@ class TestCheck_my_certs(unittest.TestCase):
         self.assertEqual(("google.com", 443), res[0])
         self.assertEqual(("yahoo.com", 8443), res[1])
 
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        help_result = runner.invoke(cli.main, ["--help"])
-        self.assertEqual(0, help_result.exit_code)
-        self.assertRegex(help_result.output, r"--help\s+Show this message and exit")
-        # result = runner.invoke(cli.main)
-        # assert result.exit_code == 0
-        # assert "check_my_certs.cli.main" in result.output
+    def test_parse_date(self):
+        """Test the parse_date function returns the proper datetime object."""
+        testdate = datetime(2023, 5, 1, tzinfo=timezone.utc)
+        parsed = check_my_certs.parse_date(
+            testdate.strftime(check_my_certs.DATE_FORMAT)
+        )
+        self.assertEqual(testdate, parsed)
+
+    def test_check_cert(self):
+        """Test the cert characterization with all socket interaction mocked out."""
+        create_def_ctx = "check_my_certs.check_my_certs.ssl.create_default_context"
+        create_conn = "check_my_certs.check_my_certs.socket.create_connection"
+
+        with mock.patch(create_def_ctx) as mock_ctx:
+            with mock.patch(create_conn):
+                return_value = dict(
+                    notBefore=(
+                        datetime.now(tz=timezone.utc) - timedelta(days=7)
+                    ).strftime(check_my_certs.DATE_FORMAT),
+                    notAfter=(
+                        datetime.now(tz=timezone.utc) + timedelta(days=30)
+                    ).strftime(check_my_certs.DATE_FORMAT),
+                )
+
+                with mock_ctx().wrap_socket() as mock_ssl_sock:
+                    mock_ssl_sock.getpeercert.return_value = return_value
+                    not_after, flag = check_my_certs.check_cert(
+                        "example.com", 443, datetime.now(tz=timezone.utc)
+                    )
+        self.assertEqual("✅", flag)
+        self.assertEqual(check_my_certs.parse_date(return_value["notAfter"]), not_after)
